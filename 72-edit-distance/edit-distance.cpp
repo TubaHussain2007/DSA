d222c57610f8d1aa892755311866298686807375
Tuba Hussain <hussaintuba12345@gmail.com>
2026-07-02 23:05:42 +0530

    Time: 7 ms (61.26%) | Memory: 13.2 MB (41.09%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-int fnc(string& s1, string& s2, int i, int j, vector<vector<int>> &dp){
-    if(i<0) return j+1;
-    if(j<0) return i+1;
-    if(dp[i][j] != -1) return dp[i][j];
-    //match
-    if(s1[i] == s2[j]){
-         return dp[i][j] =  fnc(s1, s2, i-1, j-1, dp);
-    }else { // not match
-        //insert deletion replace
-        return dp[i][j] =  1 + min(fnc(s1, s2, i, j-1, dp),
-                   min(fnc(s1, s2, i-1, j, dp), 
-                   fnc(s1, s2, i-1, j-1, dp)));
-    }
-  }
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return fnc(word1, word2, n-1, m-1, dp);
+    int minDistance(string start, string target) {
+        int n = start.size();
+        int m = target.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (start[i-1] == target[j-1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {  // not match
+                    // insert deletion replace
+                    dp[i][j] =1 + min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
