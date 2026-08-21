9246e793fe735323ebee24f87180ac531d09eda2
Tuba Hussain <hussaintuba12345@gmail.com>
2026-08-21 23:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,13 @@
 class Solution {
+    private:
+    int fnc(int n, vector<int> &dp) {
+        if(n<=1) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fnc(n-1, dp) + fnc(n-2, dp);
+    }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1, 0);
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        vector<int> dp(n+1, -1);
+        return fnc(n, dp);
     }
 };
